33cedd813f9d30585337dc18dc3314d6833a2ef1
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-02-21 03:54:10 -0800

    function-vs-method-added

---
+++ function-vs-method.js
@@ -0,0 +1,15 @@
+//function is a set of instructions that perform a task.
+//method is also a set of instructions that performs a perticular task.
+//but function is an object in js.every function has a prototype property.prototype is basically also an object. object is a collection of key and value; if the value are primitive(number,string,boolean) or another object;the value and key is considered as a property; if the the value is a function,it is called method.method are accosiated with an object.
+
+//Example to define the method
+//go in the browser ;declare an array and then console.dir it.
+// const a = [];
+// console.dir(a);
+//
+// const b = new Array(0, 1); and const b=[0,1] is same;
+// console.log(b);
+//so array is created from new keyword and Array() function.
+//so Array() is a function and it has a property which is prototype.
+//prototype is also an object and it has many properties.in that we will found keys and values;and the values is a function.so this values is called method.as like as indexof(),find(),map(),includes(),etc.so that is why we call these an array method.
+
